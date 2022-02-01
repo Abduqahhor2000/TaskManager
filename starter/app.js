08ff262588751587089ddfddd3acecf39334c77e
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2022-02-01 14:52:22 +0500

    pushed

--- starter/app.js
+++ starter/app.js
@@ -2,17 +2,17 @@ const express = require("express")
 const app = express()
 const tasks = require("./router/tasks")
 const connectDB = require("./db/connect")
+const notFound = require("./middleware/not-found")
+const errorHandlerMiddleware = require("./middleware/error-handler")
 require("dotenv").config()
 app.use(express.json())
 app.use("/api/v1/tasks", tasks)
-
-app.get("/", (req, res) => {
-    res.send("home page")
-})
-
+app.use(express.static("./public"))
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const start = async () => {
     try{
